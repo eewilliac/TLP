f6869c8d7cff307fbdf467e4dc7fa977faedbb18
unknown <eewilliac@gmail.com>
2024-02-03 12:42:08 -0500

    add cons, car, cdr functions

--- tlp.py
+++ tlp.py
@@ -13,3 +13,23 @@ def is_pair(lst):
 def is_atom(lst):
     return ((not is_null(lst)) and (not is_pair(lst)) and (not(type(lst) == list)))
 
+def car(lst):
+    if (type(lst) == list):
+        return lst[0]
+    else:
+        return []
+
+
+def cdr(lst):
+    if (type(lst) == list):
+        return lst[1:]
+    else:
+        return []
+
+
+def cons(arg1, arg2):
+    if(type(arg2) == list):
+        arg2.insert(0,arg1)
+        return arg2
+    else:
+        return []
