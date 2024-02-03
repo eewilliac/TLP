591e998394e8b11cf78425e67acdbd9a9954a27e
unknown <eewilliac@gmail.com>
2024-02-03 12:29:25 -0500

    add is_atom is_pair funcs to tlp

--- tlp.py
+++ tlp.py
@@ -1,3 +1,15 @@
 def is_null(lst):
     if(type(lst) == list):
         return len(lst) == 0
+    else:
+        return False
+
+def is_pair(lst):
+    if (type(lst) == list):
+        return (len(lst) == 2)
+    else:
+        return False
+
+def is_atom(lst):
+    return ((not is_null(lst)) and (not is_pair(lst)) and (not(type(lst) == list)))
+
